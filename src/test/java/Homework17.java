47e79716b1f6d16249f22ebf5c2fbd0bb76ea357
Salma Ayad <salma.ayad.87@gmail.com>
2022-10-27 23:00:54 -0400

    Update Homework17.java

--- src/test/java/Homework17.java
+++ src/test/java/Homework17.java
@@ -1,17 +1,25 @@
 import org.openqa.selenium.By;
+<<<<<<< Updated upstream
 import org.openqa.selenium.JavascriptExecutor;
+=======
+>>>>>>> Stashed changes
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Homework17 extends BaseTest {
     @Test
+<<<<<<< Updated upstream
     public void addSongToPlaylist() throws InterruptedException {
+=======
+        public void addSongToPlaylist() throws InterruptedException {
+>>>>>>> Stashed changes
         provideEmail("salma.ayad.87@gmail.com");
         providePassword();
         clickSubmitBtn();
         clickAllSongs();
         chooseSongClickAddTo();
+<<<<<<< Updated upstream
 
         //Choose Playlist to add the song to
         WebElement playlistDropdownSelector = driver.findElement(By.xpath("//*[@id='songsWrapper']/header/div[3]/div/section[1]/ul/li[5]"));
@@ -20,6 +28,22 @@ public class Homework17 extends BaseTest {
 
         //Clicking on the playlist
         WebElement playlistSelector = driver.findElement(By.xpath("//*[@id='playlists']/ul/li[3]/a"));
+=======
+        Thread.sleep(2000);
+
+        //Choose Playlist to add the song to
+
+       // WebElement playlistDropdownSelector = driver.findElement(By.xpath("//*[@id='songsWrapper']/header/div[3]/div/section[1]/ul/li[5]"));
+        WebElement playlistDropdownSelector = driver.findElement(By.xpath("//section[@id='songsWrapper']//ul//li[contains(text() , 'Salma New Playlist')]"));
+        //section[@id='songsWrapper']//li[contains(text(),"Salma's New Playlist")]
+        playlistDropdownSelector.click();
+
+       // WebElement myPlaylistSelector =
+        Thread.sleep(2000);
+
+        //Clicking on the playlist
+        WebElement playlistSelector = driver.findElement(By.xpath("//a[text()='Salma New Playlist']"));
+>>>>>>> Stashed changes
         playlistSelector.click();
 
         //Verifying the song is added to the playlist
@@ -36,11 +60,15 @@ public class Homework17 extends BaseTest {
     }
 
 
+<<<<<<< Updated upstream
     private void clickAllSongs() {
         WebElement clickAllSongsBtn = driver.findElement(By.xpath("//a[@class='songs']"));
         clickAllSongsBtn.click();
 
     }
+=======
+
+>>>>>>> Stashed changes
 
     @Test
     public void addSongToFavorites() throws InterruptedException {
